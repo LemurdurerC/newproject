2ace96dec0b3d4597fe752c4386dd0848fd40157
LemurdurerC <35310633+LemurdurerC@users.noreply.github.com>
2025-11-24 00:24:47 +0100

    Updated Footer.tsx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -30,14 +30,10 @@ const Footer = () => {
               <div className="flex items-center justify-center md:justify-start group">
                 <Phone className="w-5 h-5 mr-3 text-green-300 group-hover:text-green-200 transition-colors duration-200" />
                 <span className="group-hover:text-green-200 transition-colors duration-200">+33 7 82 22 83 47 
-                                        </span>
-                <span className="group-hover:text-green-200 transition-colors duration-200">
-                                          +33 6 11 43 83 42</span>
+
               </div>
               <div className="flex items-center justify-center md:justify-start group">
                 <Phone className="w-5 h-5 mr-3 text-green-300 group-hover:text-green-200 transition-colors duration-200" />
-                <span className="group-hover:text-green-200 transition-colors duration-200">+33 7 82 22 83 47 
-                                        </span>
                 <span className="group-hover:text-green-200 transition-colors duration-200">
                                           +33 6 11 43 83 42</span>
               </div>
