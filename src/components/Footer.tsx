f849965c498b246c17f60690ba4c26db8be6d2e6
LemurdurerC <35310633+LemurdurerC@users.noreply.github.com>
2025-11-24 00:25:56 +0100

    Updated Footer.tsx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -57,7 +57,7 @@ const Footer = () => {
               <div className="flex items-center justify-center">
                 <MapPin className="w-5 h-5 mr-2 text-green-300" />
                 <p className="text-purple-200">
-                  Domaine des Oliviers, Gordes
+                 Domaine de Saint-Marc, Merry-sur-Yonne
                 </p>
               </div>
               <div className="inline-flex items-center space-x-4 text-purple-300 mt-6">
