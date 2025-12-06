de9ee0fbb0d2e5524022f23023b728b912203149
LemurdurerC <35310633+LemurdurerC@users.noreply.github.com>
2025-12-06 18:54:53 +0100

    Updated Quiz.tsx

--- src/components/Quiz.tsx
+++ src/components/Quiz.tsx
@@ -237,7 +237,7 @@ const Quiz = () => {
             <p className="text-xl text-gray-600">
               {score >= 7 ? "Excellent ! Vous nous connaissez par coeur !" :
                score >= 4 ? "Bien joué ! Vous nous connaissez bien." :
-               "Pas mal ! Merci d'avoir jouer."}
+               "Pas mal ! Merci d'avoir joué."}
             </p>
           </div>
 
