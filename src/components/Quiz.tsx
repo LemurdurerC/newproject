5475c614fea0ec5ddb01d763598efb46beec9ff8
LemurdurerC <35310633+LemurdurerC@users.noreply.github.com>
2025-12-06 18:49:23 +0100

    Updated Quiz.tsx

--- src/components/Quiz.tsx
+++ src/components/Quiz.tsx
@@ -45,7 +45,7 @@ const Quiz = () => {
       correct: 2
     },
     {
-      question: "Si Simon quittait son travail, qu'est-ce qu'il préférerait ouvrir ?",
+      question: "Si Simon quittait son travail, que préférerait-il ouvrir ?",
       options: ["Un escape game", "Une cave à vodka", "Un bar à jeux", "Une librairie franco-japonaise"],
       correct: 2
     },
