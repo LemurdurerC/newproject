e396d06bd8cd84785b109237be5b40ba4caf3800
LemurdurerC <35310633+LemurdurerC@users.noreply.github.com>
2025-12-15 14:33:04 +0100

    Updated OurStory.tsx

--- src/components/OurStory.tsx
+++ src/components/OurStory.tsx
@@ -13,7 +13,7 @@ const OurStory = () => {
     code: 'FR',
     name: 'France',
     x: 33,
-    y: 65,
+    y: 79,
     image:
       'https://images.pexels.com/photos/1024993/pexels-photo-1024993.jpeg?auto=compress&cs=tinysrgb&w=800&h=600',
     flag: '🇫🇷',
