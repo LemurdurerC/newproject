2a3e84d6b18589f2c33f58d28be7bed16485fec3
LemurdurerC <35310633+LemurdurerC@users.noreply.github.com>
2025-12-15 15:02:47 +0100

    Updated OurStory.tsx

--- src/components/OurStory.tsx
+++ src/components/OurStory.tsx
@@ -209,7 +209,7 @@ const OurStory = () => {
   {
     code: 'SB',
     name: 'Serbie',
-    x: 44,
+    x: 50,
     y: 83,
     image:
       'https://images.pexels.com/photos/2901269/pexels-photo-2901269.jpeg?auto=compress&cs=tinysrgb&w=800&h=600',
