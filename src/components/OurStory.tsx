cbf9d1fbafa1c5d8c40787968f76c142542d9c83
LemurdurerC <35310633+LemurdurerC@users.noreply.github.com>
2025-12-15 14:40:33 +0100

    Updated OurStory.tsx

--- src/components/OurStory.tsx
+++ src/components/OurStory.tsx
@@ -78,7 +78,7 @@ const OurStory = () => {
     code: 'JP',
     name: 'Japan',
     x: 77,
-    y: 33,
+    y: 44,
     image:
       'https://images.pexels.com/photos/2901269/pexels-photo-2901269.jpeg?auto=compress&cs=tinysrgb&w=800&h=600',
     flag: '🇯🇵',
