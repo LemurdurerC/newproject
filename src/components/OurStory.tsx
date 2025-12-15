f4a91f1696a388a80205e4ce888195824074c9ff
LemurdurerC <35310633+LemurdurerC@users.noreply.github.com>
2025-12-15 14:59:14 +0100

    Updated OurStory.tsx

--- src/components/OurStory.tsx
+++ src/components/OurStory.tsx
@@ -162,6 +162,17 @@ const OurStory = () => {
     landmark: '🗻',
     description: 'Tradition japonaise et modernité',
   },
+  {
+    code: 'PT',
+    name: 'Portugal',
+    x: 22,
+    y: 66,
+    image:
+      'https://images.pexels.com/photos/2901269/pexels-photo-2901269.jpeg?auto=compress&cs=tinysrgb&w=800&h=600',
+    flag: '🇯🇵',
+    landmark: '🗻',
+    description: 'Tradition japonaise et modernité',
+  },
 ];
 
 
