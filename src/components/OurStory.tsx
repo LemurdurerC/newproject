f337afc247fe755bf649f8f4b9d1033f97b45834
LemurdurerC <35310633+LemurdurerC@users.noreply.github.com>
2025-12-15 14:57:59 +0100

    Updated OurStory.tsx

--- src/components/OurStory.tsx
+++ src/components/OurStory.tsx
@@ -143,7 +143,7 @@ const OurStory = () => {
   {
     code: 'MCN',
     name: 'Macédoine du Nord',
-    x: 44,
+    x: 55,
     y: 81,
     image:
       'https://images.pexels.com/photos/2901269/pexels-photo-2901269.jpeg?auto=compress&cs=tinysrgb&w=800&h=600',
