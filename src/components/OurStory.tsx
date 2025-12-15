fff173ad829ba6ab3334e2dfe07553512abfc92d
LemurdurerC <35310633+LemurdurerC@users.noreply.github.com>
2025-12-15 14:56:32 +0100

    Updated OurStory.tsx

--- src/components/OurStory.tsx
+++ src/components/OurStory.tsx
@@ -133,7 +133,7 @@ const OurStory = () => {
     code: 'MN',
     name: 'Monténégro',
     x: 44,
-    y: 83,
+    y: 81,
     image:
       'https://images.pexels.com/photos/2901269/pexels-photo-2901269.jpeg?auto=compress&cs=tinysrgb&w=800&h=600',
     flag: '🇯🇵',
