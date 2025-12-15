e214d309f5c241c62977a850fdb1021293066ed0
LemurdurerC <35310633+LemurdurerC@users.noreply.github.com>
2025-12-15 14:47:04 +0100

    Updated OurStory.tsx

--- src/components/OurStory.tsx
+++ src/components/OurStory.tsx
@@ -96,6 +96,17 @@ const OurStory = () => {
     landmark: '🗻',
     description: 'Tradition japonaise et modernité',
   },
+  {
+    code: 'RT',
+    name: 'Tchéquie',
+    x: 44,
+    y: 84,
+    image:
+      'https://images.pexels.com/photos/2901269/pexels-photo-2901269.jpeg?auto=compress&cs=tinysrgb&w=800&h=600',
+    flag: '🇯🇵',
+    landmark: '🗻',
+    description: 'Tradition japonaise et modernité',
+  },
 ];
 
 
